ad2ff9bd70db473bc16444a01f8d7c278c92e75f
nesdown <newsdownloads12@gmail.com>
2018-11-26 13:29:22 +0200

    Add first code part

---
+++ src/FirstLab.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class FirstLab {
+
+    private int[][] array;
+    private int n;
+    private int m;
+
+    FirstLab() throws IOException {
+        outName();
+
+
+        System.out.println("Enter n value: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(reader.readLine());
+
+        System.out.println("Enter m value: ");
+        m = Integer.parseInt(reader.readLine());
+
+        array = new int[n][n];
+
+
+        fulfillArray();
+
+        printArray(array);
+        shiftArray();
+        printArray(array);
+    }
+
+    private void fulfillArray() {
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 10);
+            }
+        }
+    }
+
+    private void printArray(int[][] array) {
+        for (int[] anArray : array) {
+            for (int anAnArray : anArray) {
+                System.out.print(anAnArray + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
+    private void shiftArrayOnce() {
+        int[] temp = array[array.length - 1];
+
+        for(int i = array.length - 1; i > 0; i--){
+
+            array[i]=array[i-1];   // do the switch
+
+        }
+
+        // do the switch
+//        if (array.length - 1 >= 0) System.arraycopy(array, 0, array, 1, array.length - 1);
+
+        array[0] = temp;
+    }
+
+    private void shiftArray() {
+        for (int i = 0; i < m; i++) shiftArrayOnce();
+    }
+
+    private void outName() {
+        System.out.println("Hlushko B.S.");
+        System.out.println();
+    }
+
+
+}
